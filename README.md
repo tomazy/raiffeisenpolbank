4cdfe2277ffa5ce192f952d9ee39f07fc2fe3e85
tomazy <tomazy@go2.pl>
2017-04-01 18:31:58 +0800

    Added cron job support script

--- README.md
+++ README.md
@@ -4,18 +4,24 @@ Tabele kursów CHF Raiffeisen/Polbank
 Skrypty do generowania tabel historycznych kursów CHF w Raiffeisen/Polbank
 
 ```bash
-npm -s start -- 2007-01-01 2008-01-01 | sort | uniq > 2007.csv
+npm -s bank -- 2007-01-01 2008-01-01 | sort | uniq > 2007.csv
 
 # or
 
-npm -s start -- 2017 | sort | uniq # scrapes from 2017-01-01 up to yesterday
+npm -s bank -- 2017 | sort | uniq # scrapes from 2017-01-01 up to yesterday
 
 ```
 
 Tabele kursów są generowane przez scrape'wanie stron Raiffeisen/Polbank.
 
-Strony Raiffeisen/Polbank są cache'owane w katalogu `.cache`
+Wszystkie odpowiedzi (responses) http są cache'owane w katalogu `.cache`.
 
+Aktualizacja wszystkich kursów
+------------------------------
+
+```bash
+npm run update-rates
+```
 
 Libor 3M
 --------
---
+++ scripts/update-rates.js
@@ -0,0 +1,50 @@
+import * as shell from 'shelljs'
+import * as path from 'path'
+import startOfYesterday from 'date-fns/start_of_yesterday'
+import format from 'date-fns/format'
+
+shell.config.fatal = true // set -e
+shell.config.silent = true
+
+const debug = require('debug')('update-rates')
+
+const DATE_FORMAT = 'YYYY-MM-DD'
+const yesterday = format(startOfYesterday(), DATE_FORMAT)
+
+const year = (new Date()).getFullYear()
+const dataDir = path.resolve('data')
+
+function fetchBankRates () {
+  const ratesPath = path.join(dataDir, `${year}.csv`)
+  debug('bank rates file', ratesPath)
+
+  shell
+    .exec(`npm -s run bank -- ${year}`)
+    .exec('sort')
+    .exec('uniq')
+    .to(ratesPath)
+}
+
+function fetchLibor () {
+  const ratesPath = path.join(dataDir, `libor-3m-chf-${year}.csv`)
+  debug('libor rates file', ratesPath)
+
+  shell
+    .exec(`npm run -s libor -- ${year}-01-01 ${yesterday}`)
+    .to(ratesPath)
+}
+
+function fetchNBP () {
+  const ratesPath = path.join(dataDir, `nbp-${year}.csv`)
+  debug('nbp rates file', ratesPath)
+
+  shell
+    .exec(`npm run -s nbp -- ${year}`)
+    .to(ratesPath)
+}
+
+fetchBankRates()
+fetchLibor()
+fetchNBP()
+
+debug('done!')
